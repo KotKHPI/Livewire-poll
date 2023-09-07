cf4786e6e270ff5e93756c481d74f6724de26d4b
KotKHPI <prototype2428@gmail.com>
2023-09-07 11:46:34 +0300

    Add validation

--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -5,6 +5,9 @@
         <input type="text" wire:model.live="title" />
 
         Current title: {{ $title }}
+        @error('title')
+            <div class="text-red-500">{{ $message }}</div>
+        @enderror
 
         <div class="mb-4 mt-4">
             <button class="btn" wire:click.prevent="addOption">Add option</button>
@@ -18,6 +21,9 @@
                         <input type="text" wire:model="options.{{ $index }}">
                         <button class="btn" wire:click.prevent="removeOption( {{$index}} )">Remove</button>
                     </div>
+                    @error("options.{$index}")
+                        <div class="text-red-500">{{ $message }}</div>
+                    @enderror
                 </div>
 
             @endforeach
