1f955f9a746c9ec01e0e82571316e3618801a8ce
KotKHPI <prototype2428@gmail.com>
2023-09-07 10:46:09 +0300

    Add form for crate poll and make function for create poll

--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -1,22 +1,29 @@
 <div>
-    <form>
+    <form wire:submit.prevent="createPoll">
         <label>Poll Title</label>
 
         <input type="text" wire:model.live="title" />
 
         Current title: {{ $title }}
 
-        <div>
+        <div class="mb-4 mt-4">
             <button class="btn" wire:click.prevent="addOption">Add option</button>
         </div>
 
         <div>
             @foreach($options as $index => $option)
-                <div>
-                    {{$index}} - {{$option}}
+                <div class="mb-4">
+                    <label>Option {{$index + 1}}</label>
+                    <div class="flex gap-2">
+                        <input type="text" wire:model="options.{{ $index }}">
+                        <button class="btn" wire:click.prevent="removeOption( {{$index}} )">Remove</button>
+                    </div>
                 </div>
+
             @endforeach
         </div>
+
+        <button class="btn" type="submit">Create poll!</button>
 
     </form>
 </div>
